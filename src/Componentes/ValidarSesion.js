64d16fe9e9945820c07319f594dd23f72a027057
Juan Pestana <juanpestana96@gmail.com>
2020-12-03 10:39:44 -0500

    Cambio de nombres e iconos

--- src/Componentes/ValidarSesion.js
+++ src/Componentes/ValidarSesion.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import {Text,Image,View} from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage'
-import {SET} from 'Redux/actions/Usuario'
-import {connect} from 'react-redux'
-import {ROUTERS} from "utils/navigation";
+import React from 'react';
+import {Text, Image, View} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import {SET} from 'Redux/actions/Usuario';
+import {connect} from 'react-redux';
+import {ROUTERS} from 'utils/navigation';
 import LottieView from 'lottie-react-native';
 
-
-class ValidarSesion extends React.Component{
-
-    async componentDidMount(){
-        const token= await AsyncStorage.getItem('token')
-        if(token){
-            let user=await AsyncStorage.getItem('user')
-            user=JSON.parse(user)
-            this.props.SET(user)
-            global.ordering.setAccessToken(token)
-            setTimeout(()=>{
-                this.props.navigation.navigate('Inicio');
-            },2000)
-            
-        }else{
-            setTimeout(()=>{
-                this.props.navigation.navigate(ROUTERS.SignIn);
-            },2000)
-        }
-    }
-    render(){
-        return (<View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <LottieView autoPlay loop={false} autoSize style={{width:150}} source={require('Animaciones/logo2.json')}/>
-        </View>)
+class ValidarSesion extends React.Component {
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      let user = await AsyncStorage.getItem('user');
+      user = JSON.parse(user);
+      this.props.SET(user);
+      global.ordering.setAccessToken(token);
+      setTimeout(() => {
+        this.props.navigation.navigate('Inicio');
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        this.props.navigation.navigate(ROUTERS.SignIn);
+      }, 2000);
     }
   }
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Image
+          source={require('imagenes/logo.png')}
+          style={{
+            marginTop: 20,
+            alignSelf: 'center',
+            height: 200,
+            width: 150,
+            resizeMode: 'contain',
+          }}></Image>
+      </View>
+    );
+  }
+}
 
 const mapearEstado = (state) => {
   return {
